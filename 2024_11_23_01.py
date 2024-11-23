249bd6a77eb408d2608730bb928b168f0d8cde3e
StarFinder <hubowen@stu.zzu.edu.cn>
2024-11-23 20:55:30 +0800

    Update 2024_11_23_01.py

--- 2024_11_23_01.py
+++ 2024_11_23_01.py
@@ -7,7 +7,7 @@ for name , fav_thing in fav_inv.items():
     print(f"{name} like {fav_thing['place']},favnum is {fav_thing['fav_num']}")
 
 """
-´íÎóÑù±¾£º
+é”™è¯¯æ¡ˆä¾‹
 fav_inv = {
     'lihua':{'place':'england','fav_num':6},
     'BOWEN':{'place':'DPRK','fav_num':3}
