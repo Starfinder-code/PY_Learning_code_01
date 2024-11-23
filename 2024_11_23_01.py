6270edfadfd72b5373f50c914e8762632d520eca
Starfinder-code <hubowen@stu.zzu.edu.cn>
2024-11-23 20:37:18 +0800

    2024_11_23_01sub

---
+++ 2024_11_23_01.py
@@ -0,0 +1,19 @@
+fav_inv = {
+    'lihua':{'place':'england','fav_num':6},
+    'BOWEN':{'place':'DPRK','fav_num':3}
+    }
+
+for name , fav_thing in fav_inv.items():
+    print(f"{name} like {fav_thing['place']},favnum is {fav_thing['fav_num']}")
+
+"""
+´íÎóÑù±¾£º
+fav_inv = {
+    'lihua':{'place':'england','fav_num':6},
+    'BOWEN':{'place':'DPRK','fav_num':3}
+    }
+
+for name , fav_thing in fav_inv:
+    print(f'{name} like {fav_thing['place']},favnum is {fav_thing['fav_num']} ')
+ 
+"""
